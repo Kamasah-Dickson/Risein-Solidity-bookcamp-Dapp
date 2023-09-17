098f7e5d3332d5a6e3f81bb49da0ff5f8df52d6c
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-09-17 05:07:41 +0000

    add data

---
+++ frontend/data.ts
@@ -0,0 +1,43 @@
+import icon1 from "../public/images/icons/icon1.svg";
+import icon2 from "../public/images/icons/icon2.svg";
+import icon3 from "../public/images/icons/icon3.svg";
+import icon4 from "../public/images/icons/icon4.svg";
+import icon5 from "../public/images/icons/icon5.svg";
+import icon6 from "../public/images/icons/icon6.svg";
+import icon7 from "../public/images/icons/icon7.svg";
+import icon8 from "../public/images/icons/icon8.svg";
+
+export const iconData = [
+	{
+		image: icon1,
+		id: "23423",
+	},
+	{
+		image: icon2,
+		id: "23dfsd3",
+	},
+	{
+		image: icon3,
+		id: "23esd3",
+	},
+	{
+		image: icon4,
+		id: "2r3sd3",
+	},
+	{
+		image: icon5,
+		id: "2rr3",
+	},
+	{
+		image: icon6,
+		id: "2rrdr3",
+	},
+	{
+		image: icon7,
+		id: "2rrd3dr3",
+	},
+	{
+		image: icon8,
+		id: "2r2dr3",
+	},
+];
