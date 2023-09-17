b9fb9f6e44f181326da3acf28002d0bbab470743
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-09-17 05:11:30 +0000

    update data path

--- frontend/data.ts
+++ frontend/data.ts
@@ -1,11 +1,11 @@
-import icon1 from "../public/images/icons/icon1.svg";
-import icon2 from "../public/images/icons/icon2.svg";
-import icon3 from "../public/images/icons/icon3.svg";
-import icon4 from "../public/images/icons/icon4.svg";
-import icon5 from "../public/images/icons/icon5.svg";
-import icon6 from "../public/images/icons/icon6.svg";
-import icon7 from "../public/images/icons/icon7.svg";
-import icon8 from "../public/images/icons/icon8.svg";
+import icon1 from "./public/images/icons/icon1.svg";
+import icon2 from "./public/images/icons/icon2.svg";
+import icon3 from "./public/images/icons/icon3.svg";
+import icon4 from "./public/images/icons/icon4.svg";
+import icon5 from "./public/images/icons/icon5.svg";
+import icon6 from "./public/images/icons/icon6.svg";
+import icon7 from "./public/images/icons/icon7.svg";
+import icon8 from "./public/images/icons/icon8.svg";
 
 export const iconData = [
 	{
