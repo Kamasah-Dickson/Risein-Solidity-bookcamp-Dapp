ac6e3ece838300f57adb85b1ef24235a9c0bff34
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-09-17 05:08:56 +0000

    add rainbow client

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,51 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
+import ProgressBar from "nextjs-progressbar";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+	getDefaultWallets,
+	RainbowKitProvider,
+	darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, bscTestnet } from "wagmi/chains";
+
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains(
+	[bscTestnet, mainnet, polygon, optimism, arbitrum],
+	[
+		alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID! }),
+		publicProvider(),
+	]
+);
+const { connectors } = getDefaultWallets({
+	appName: "My RainbowKit App",
+	projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECTID!,
+	chains,
+});
+const wagmiConfig = createConfig({
+	autoConnect: true,
+	connectors,
+	publicClient,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<ProgressBar color="#5150E7" height={3} />
+			<WagmiConfig config={wagmiConfig}>
+				<RainbowKitProvider
+					chains={chains}
+					theme={darkTheme({
+						accentColor: "#5150E7",
+					})}
+				>
+					<Component {...pageProps} />
+				</RainbowKitProvider>
+			</WagmiConfig>
+		</>
+	);
 }
