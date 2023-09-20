49b1c5468fbe0b04b0b1ba5f777c21e996f07eef
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-09-20 03:25:20 +0000

    update solidity frontend

---
+++ frontend/constants/index.ts
@@ -0,0 +1,4 @@
+import abi from "./abi.json";
+import contractAddress from "./contractAddressess.json";
+
+export { abi, contractAddress };
---
+++ solidity/scripts/update-frontend.js
@@ -0,0 +1,55 @@
+const { ethers, network } = require("hardhat");
+
+const { readFileSync, writeFileSync } = require("fs");
+require("dotenv").config({ path: "../.env" });
+
+const FRONT_END_ADDRESSES_FILE =
+	"../frontend/constants/contractAddressess.json";
+
+const FRONT_END_ABI_FILE = "../frontend/constants/abi.json";
+const UPDATE_FRONT_END = process.env.UPDATE_FRONT_END;
+
+async function updateFrontend() {
+	if (UPDATE_FRONT_END) {
+		await updateContractAddressess();
+		await updateAbi();
+		console.log("Frontend updated✨");
+	}
+}
+
+module.exports = updateFrontend;
+
+async function updateAbi() {
+	const LockTokenFactory = await ethers.getContractFactory("LockToken");
+	const lockToken = await LockTokenFactory.deploy();
+	await lockToken.waitForDeployment();
+
+	await lockToken.getAddress();
+	writeFileSync(
+		FRONT_END_ABI_FILE,
+		JSON.stringify(LockTokenFactory.interface.fragments)
+	);
+}
+
+async function updateContractAddressess() {
+	const LockTokenFactory = await ethers.getContractFactory("LockToken");
+	const lockToken = await LockTokenFactory.deploy();
+	await lockToken.waitForDeployment();
+
+	const chainId = network.config.chainId.toString();
+	const currentAddresses = JSON.parse(
+		readFileSync(FRONT_END_ADDRESSES_FILE, "utf8")
+	);
+
+	if (chainId in currentAddresses) {
+		if (!currentAddresses[chainId].includes(await lockToken.getAddress())) {
+			currentAddresses[chainId].push(await lockToken.getAddress());
+		}
+	} else {
+		currentAddresses[chainId] = [await lockToken.getAddress()];
+	}
+
+	writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(currentAddresses));
+}
+
+module.exports.tags = ["frontend"];
