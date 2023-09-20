f1822751f5ee9f0199972fbc1fd51ec401034967
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-09-20 02:38:08 +0000

    update header

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -5,12 +5,12 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const Header = () => {
 	return (
-		<header className="sticky top-0 mt-7 left-0 w-full  block z-20 ">
-			<div className="bg-[rgba(255,255,255,0.1)] px-7 flex items-center justify-between py-4 rounded-full my-max">
+		<header className="fixed top-0 mt-7 left-0 w-full  block z-20 ">
+			<div className="bg-[rgba(255,255,255,0.1)] px-7 backdrop-blur-[3px] saturate-50 flex items-center justify-between py-4 rounded-full my-max">
 				<Link href="/" className="w-fit h-fit">
 					<Image width={25} height={25} src={handImage} alt="" />
 				</Link>
-				
+
 				<ConnectButton />
 			</div>
 		</header>
