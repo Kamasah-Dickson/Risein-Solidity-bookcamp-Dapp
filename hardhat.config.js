f6c541461510589b69433e53d2d22670eda91a5f
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-09-20 11:57:18 +0000

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
-// require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-// require("hardhat-deploy");
 require("dotenv").config();
 
-const BNBCHAIN_RPC_URL =
-	process.env.BNBCHAIN_RPC_URL || "https://bsc-dataseed1.binance.org/"; // Replace with the actual BSC RPC URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const BINANCESCAN_API_KEY =
-	process.env.BINANCESCAN_API_KEY || "Your BscScan API key";
+const BINANCESCAN_API_KEY = process.env.BINANCESCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 module.exports = {
 	solidity: "0.8.9",
-	// defaultNetwork: "bnbchain",
+	defaultNetwork: "bnbchain",
 	networks: {
 		bnbchain: {
-			url: BNBCHAIN_RPC_URL,
-			accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-			// saveDeployments: true,
+			url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+			accounts: [PRIVATE_KEY],
 			chainId: 97,
 		},
 	},
+
+	gasReporter: {
+		enabled: true,
+		currency: "BNB",
+		// noColors: true,
+		coinmarketcap: COINMARKETCAP_API_KEY,
+		token: "BNB",
+	},
+
 	etherscan: {
 		// yarn hardhat verify --network <NETWORK>
 		apiKey: BINANCESCAN_API_KEY,
