565b2f8b92cc3e8c639708e3d6f3394996fecc2c
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-09-26 03:59:38 +0000

    refactore and update withdraw component

--- frontend/components/Withdraw.tsx
+++ frontend/components/Withdraw.tsx
@@ -1,13 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, FormEvent } from "react";
 import { abi, contractAddress as contractAddresses } from "../constants/index";
+import {
+	useAccount,
+	useNetwork,
+	useContractWrite,
+	useWaitForTransaction,
+} from "wagmi";
+import { Iwithdraw } from "@/interface/interface";
 
-import { useAccount, useNetwork } from "wagmi";
-import { parseEther } from "ethers";
-import { toast } from "react-toastify";
-
-export function Withdraw({ duration }: { duration: string }) {
+export function Withdraw({
+	duration,
+	enableWithdraw,
+	setSteps,
+	setCurrentActiveStep,
+}: Iwithdraw) {
 	const { chain } = useNetwork();
-	const [holdDuration, setHoldDuration] = useState(Number(duration));
+	const [holdDuration, setHoldDuration] = useState(Number(duration) + 5);
 	const lockTokenAddress =
 		(chain?.id as number) in contractAddresses
 			? contractAddresses[
@@ -20,48 +28,90 @@ export function Withdraw({ duration }: { duration: string }) {
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			setHoldDuration(holdDuration - 1);
-			console.log(holdDuration);
+			if (holdDuration == 0) {
+				return;
+			} else {
+				setHoldDuration(holdDuration - 1);
+			}
 		}, 1000);
 
-		return clearInterval(interval);
+		return () => clearInterval(interval);
 	}, [holdDuration]);
 
-	// if (isError) {
-	// 	toast.error(isError, {
-	// 		position: "top-center",
-	// 	});
-	// }
+	const { data, isLoading, write } = useContractWrite({
+		address: lockTokenAddress as any,
+		abi,
+		functionName: "withdraw",
+		chainId: 97,
+		account: address,
+		gas: BigInt("308735"),
+		gasPrice: BigInt("1000000000"),
+		onError(error) {
+			console.log(error.message);
+			setError(error.message);
+		},
+		onSuccess(data) {
+			console.log(`Success ${data}`);
+		},
+	});
 
-	const handleWithdraw = () => {};
+	const { isLoading: isWaitForTransLoading } = useWaitForTransaction({
+		hash: data?.hash,
+		chainId: 97,
+		onSuccess(data) {
+			setCurrentActiveStep(1);
+			setSteps([
+				{ completed: false, step: 1 },
+				{ completed: false, step: 2 },
+			]);
+		},
+	});
+
+	const handleWithdraw = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		write?.();
+	};
 
 	return (
-		<form
-			className="bg-[#40404421] shadow-sm backdrop-blur-sm mt-7 md:mb-20 rounded-xl flex flex-col w-full gap-5 p-7 max-w-md mx-auto"
-			onSubmit={(e) => {
-				e.preventDefault();
-				// sendTransaction?.();
-			}}
-		>
-			<div className="text-center">{`${holdDuration}s remaining`}</div>
-			<button
-				onClick={handleWithdraw}
-				className="rounded-md cursor-pointer bg-[#4040e0] active:scale-[1.01] px-2 py-3 font-medium text-lg disabled:active:scale-[unset] disabled:cursor-not-allowed disabled:bg-[#3f3c3c]"
-				// type="submit"
-				// disabled={isLoading || !rate || !duration || Boolean(error)}
+		<div className="flex flex-col w-full">
+			<form
+				className="bg-[#40404421] shadow-sm backdrop-blur-sm mt-7 md:mb-20 rounded-xl flex flex-col w-full gap-5 p-7 max-w-md mx-auto"
+				onSubmit={(e) => handleWithdraw(e)}
 			>
-				{/* {isLoading ? "Sending..." : "Send"} */}
-				Withdraw
-			</button>
-			{/* {isSuccess && (
-				<div>
-					Successfully deployed at
+				{isWaitForTransLoading ? (
+					<div className="text-green-500">Please wait...</div>
+				) : (
+					<div className="text-center">{`${holdDuration}s remaining`}</div>
+				)}
+				<button
+					className="rounded-md cursor-pointer bg-[#4040e0] active:scale-[1.01] px-2 py-3 font-medium text-lg disabled:active:scale-[unset] disabled:cursor-not-allowed disabled:bg-[#3f3c3c]"
+					type="submit"
+					disabled={
+						enableWithdraw || holdDuration !== 0 || isWaitForTransLoading
+					}
+				>
+					{isLoading
+						? "Please wait..."
+						: isWaitForTransLoading
+						? "Pending Transaction"
+						: "Withdraw"}
+				</button>
+			</form>
+			<div className="pb-4 px-4">
+				{error && (
 					<div>
-						<a href={`https://etherscan.io/tx/${data?.hash}`}>Etherscan</a>
+						<div className=" text-center w-full">
+							<span
+								style={{ overflowWrap: "anywhere" }}
+								className="text-xs w-full text-[crimson]"
+							>
+								{error}
+							</span>
+						</div>
 					</div>
-				</div>
-			)} */}
-		</form>
+				)}
+			</div>
+		</div>
 	);
 }
 export default Withdraw;
