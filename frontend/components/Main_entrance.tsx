6512e345e570c125ad698ceaa2420dd3383496cc
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-09-26 03:50:27 +0000

    refactor Main_entrance

--- frontend/components/Main_entrance.tsx
+++ frontend/components/Main_entrance.tsx
@@ -1,43 +1,36 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import LockToken from "../components/LockToken";
 import { SendTransaction } from "./SendTransaction";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useAccount } from "wagmi";
 import Withdraw from "./Withdraw";
+import { LoadingContext } from "@/context/loadingContext";
+import { Isteps } from "@/interface/interface";
 
-interface Isteps {
-	completed: boolean;
-	step: number;
-}
 const Main_entrance = () => {
 	const { address } = useAccount();
-	const [transError, setTransError] = useState([{ message: "", id: "" }]);
 
 	const [steps, setSteps] = useState<Isteps[]>([
 		{ completed: false, step: 1 },
 		{ completed: false, step: 2 },
-		{ completed: false, step: 3 },
 	]);
-	const [duration, setDuration] = useState("");
 
-	const [currentActiveStep, setCurrentActiveStep] = useState(2);
+	const [duration, setDuration] = useState("");
+	const { enableWithdraw, setEnableWithdraw } = useContext(LoadingContext);
+	const [currentActiveStep, setCurrentActiveStep] = useState(1);
+
 	const updateSteps = ({ step }: { completed: boolean; step: number }) => {
 		const findStep = steps.find((targetStep) => targetStep.step === step);
-		const findCompletedSteps = steps.filter((allSteps) => allSteps.completed);
 
 		if (findStep?.step === 1) {
 			setCurrentActiveStep(findStep.step);
 		} else if (findStep?.step && findStep.completed) {
 			setCurrentActiveStep(findStep.step);
 		} else {
-			setCurrentActiveStep(findCompletedSteps.length + 1);
+			return;
 		}
 	};
 
 	const goTonextStep = () => {
-		// currentActiveStep
-		// setSteps
-
 		setSteps((prev) =>
 			prev.map((prevSteps) => {
 				const currentTargetStep = prev.find(
@@ -82,10 +75,8 @@ const Main_entrance = () => {
 			</div>
 			<p className="mt-7 max-w-xs text-center">
 				{currentActiveStep === 1
-					? "First you must make a deposit of at least 0.01BNB to be able to interact with the contract"
+					? "First you must make a deposit of at least 0.01BNB to be able to interact with the contract and its functions"
 					: currentActiveStep === 2
-					? "Good luck and be expecting some amazing profits!"
-					: currentActiveStep === 3
 					? "Congratulations✨ you are about to get some amazing rewards!🎉"
 					: ""}
 			</p>
@@ -93,15 +84,19 @@ const Main_entrance = () => {
 				className={`flex mt-5 flex-col md:flex-row items-start md:gap-20 w-full max-w-3xl mx-auto`}
 			>
 				{currentActiveStep == 1 ? (
-					<SendTransaction goTonextStep={goTonextStep} />
-				) : currentActiveStep == 2 ? (
-					<LockToken
+					<SendTransaction
+						setEnableWithdraw={setEnableWithdraw}
 						goTonextStep={goTonextStep}
 						duration={duration}
 						setDuration={setDuration}
 					/>
-				) : currentActiveStep == 3 ? (
-					<Withdraw duration={duration} />
+				) : currentActiveStep == 2 ? (
+					<Withdraw
+						duration={duration}
+						enableWithdraw={enableWithdraw}
+						setSteps={setSteps}
+						setCurrentActiveStep={setCurrentActiveStep}
+					/>
 				) : null}
 
 				{(!currentActiveStep as unknown as number) === 3 ? (
